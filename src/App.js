d7225722b84a9da833bbfb3c68360f4828f882b0
MandyNguyen2002 <mandynguyen0611@gmail.com>
2024-06-24 22:19:52 -0400

    update full code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import ToDoList from './ToDoList';
+import { TextField, Button, Checkbox, List, ListItem, Container, Box} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='to-do-list'>
+      <h1>TO DO LIST</h1>
+      <ToDoList/> 
     </div>
+    
+  
+    
+    
   );
 }
 
